cabfa0d244cd9449f0043d727f4cfc194deb6de4
slimerence <39113678+slimerence@users.noreply.github.com>
2019-05-02 00:49:15 +1000

    init repo

---
+++ resources/views/elements/head.blade.php
@@ -0,0 +1,27 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Vitality - Multipurpose Bootstrap 4 One Page Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom fonts for this theme -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,600,500,700,800,900' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css">
+    <link href="vendor/owl-carousel/owl.theme.css" rel="stylesheet" type="text/css">
+    <link href="vendor/owl-carousel/owl.transitions.css" rel="stylesheet" type="text/css">
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+    <link href="vendor/animate.css/animate.min.css" rel="stylesheet" type="text/css">
+    <link href="device-mockups/device-mockups.min.css" rel="stylesheet" type="text/css">
+
+    <link href="{{ url('css/mozi.css') }}" rel="stylesheet" type="text/css">
+
+</head>
---
+++ resources/views/elements/js.blade.php
@@ -0,0 +1,18 @@
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="vendor/owl-carousel/owl.carousel.js"></script>
+<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="vendor/vide/jquery.vide.min.js"></script>
+<script src="vendor/mixitup/mixitup.min.js"></script>
+<script src="vendor/wowjs/wow.min.js"></script>
+
+<!-- Contact form JavaScript -->
+<script src="js/contact_me.js"></script>
+<script src="js/jqBootstrapValidation.js"></script>
+<!-- Custom scripts for this theme -->
+<script src="js/vitality.js"></script>
+<script src="js/mixitup-config.js"></script>
