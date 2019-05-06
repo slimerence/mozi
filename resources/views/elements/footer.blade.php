d07695ce9cf502f69e8ee6ee80d402c18f264b4a
slimerence <39113678+slimerence@users.noreply.github.com>
2019-05-06 13:54:08 +1000

    Update footer.blade.php

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -8,16 +8,16 @@
                 <a href="mailto:sales@atoptechnology.com">sales@atoptechnology.com</a>
             </div>
             <div class="col-md-4 offset-md-1 my-footer">
-                <p>North America</p>
+                <p style="line-height: 3em;">North America</p>
                 <p>377 Chapman Ave.
                     <br>Suite 235 Placentia CA 92870</p>
-                <p>555-213-4567</p>
+                <p>+1 657 444 9015</p>
             </div>
             <div class="col-md-4 my-footer">
-                <p>North America</p>
-                <p>377 Chapman Ave.
-                    <br>Suite 235 Placentia CA 92870</p>
-                <p>555-213-4567</p>
+                <p>South East Asia, Australia & New Zealand</p>
+                <p>409/685 Burke Road.
+                    <br>Camberwell VIC 3124</p>
+                <p>+61 3 9827 1454</p>
             </div>
         </div>
 
