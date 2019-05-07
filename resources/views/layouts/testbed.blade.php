24b763c0065dfff38db37fab001ea556aadfa4da
slimerence <39113678+slimerence@users.noreply.github.com>
2019-05-07 12:26:32 +1000

    Update testbed.blade.php

--- resources/views/layouts/testbed.blade.php
+++ resources/views/layouts/testbed.blade.php
@@ -7,14 +7,10 @@
     <div class="container text-center wow fadeIn" style="padding: 150px 0;">
         <div class="row">
             <div class="col-md-10 mx-auto">
-                <h2>TEST <span class="blue_title">BED</span></h2>
-                <p class="mb-0 mt-50">Quality makes us different,
-                    our in house chipset level production line work through a strickt and smooth progress to esure every product reach
-                    a high level performance. Our below tests with every product 100% of the time.</p>
+                <h2>Compatibility</h2>
+                <p class="mb-0 mt-50" style="font-size: 28px;">Code makes the difference</p>
                 <hr class="colored mt-50">
-                <p class="mb-0 mx-auto mt-50" style="max-width: 600px;">Quality makes us different,
-                    our in house chipset level production line work through a strickt and smooth progress to esure every product reach
-                    a high level performance. Our below tests with every product 100% of the time.</p>
+                <p class="mb-0 mx-auto mt-50" style="max-width: 600px;">We generate our products with a pefect EEPROM to work with most OEM vendors. We tested our products based on different system and equipment, therefore, we understand how to supply the best 3rd party compatible modules.</p>
                 <a class="btn btn-primary js-scroll-trigger mt-50" href="#contact">How We Work</a>
             </div>
         </div>
